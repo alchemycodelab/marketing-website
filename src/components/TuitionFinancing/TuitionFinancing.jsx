4a8f4e28389411f6b0e57ae70a15944581f4a3f1
Marty Nelson <marty@martypdx.com>
2022-01-27 14:53:31 -0800

    Add trailer content to TuitionFinancing section

--- src/components/TuitionFinancing/TuitionFinancing.jsx
+++ src/components/TuitionFinancing/TuitionFinancing.jsx
@@ -1,52 +1,65 @@
 import Section from '../common/Section';
-// import { Content, TertiaryContent } from '../content/Content';
-// import { Title } from '../content/Text';
+import { Content, TertiaryContent } from '../content/Content';
+import { Title } from '../content/Text';
 import styles from './TuitionFinancing.module.scss';
 
 export default function TuitionFinancing({ 
   content,
   title,
   sources = [],
+  trailer = {},
   ...rest }) {
-
+  
   return (
     <Section {...rest} className={styles.TuitionFinancing}>
-      {/* <div className={styles.contentContainer}>
+      <div className={styles.contentContainer}>
         <Content content={content} />
-      </div> */}
+      </div>
 
-      {/* <Title text={title}/> */}
+      <Title text={title}/>
       
-      {/* <ol className={styles.sources}>
+      <ol className={styles.sources}>
         {sources.map((source, index) => <Source key={index} source={source}/>)}
-      </ol> */}
+      </ol>
 
+
+      <div className={styles.trailerContainer}>
+        <TertiaryContent content={trailer.content} />
+      </div>
     </Section>
   );
 }
 
-// function Source({ source }) {
-//   return (
-//     <li className={styles.source}>
-//       <TertiaryContent content={source.content} />
-//     </li>
-//   );
-// }
+function Source({ source }) {
+  return (
+    <li className={styles.source}>
+      <TertiaryContent content={source.content} />
+    </li>
+  );
+}
 
 
-// TuitionFinancing.config = {
-//   name: 'Tuition Financing',
-//   inputs: [
-//     ...Section.inputs,
-//     ...Content.inputs,
-//     Title.input,
-//     {
-//       name: 'sources',
-//       type: 'list',
-//       defaultValue: [],
-//       subFields: [
-//         ...Content.inputs,
-//       ]
-//     },
-//   ]
-// };
+TuitionFinancing.config = {
+  name: 'Tuition Financing',
+  inputs: [
+    ...Section.inputs,
+    ...Content.inputs,
+    Title.input,
+    {
+      name: 'sources',
+      type: 'list',
+      defaultValue: [],
+      subFields: [
+        ...Content.inputs,
+      ]
+    },
+    {
+      name: 'trailer',
+      type: 'object',
+      defaultValue: {},
+      subFields: [
+        ...Content.inputs,
+      ]
+    }
+  ]
+};
