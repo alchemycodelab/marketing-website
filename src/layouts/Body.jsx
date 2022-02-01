de6908a4fdeccc4829d47f5c82c19e1124f23e38
Marty Nelson <marty@martypdx.com>
2022-02-01 10:04:11 -0800

    move global css to <link>

--- src/layouts/Body.jsx
+++ src/layouts/Body.jsx
@@ -1,18 +1,20 @@
-import HeaderBar from '../components/HeaderBar/HeaderBar.jsx'
+ import HeaderBar from '../components/HeaderBar/HeaderBar.jsx'
 import Footer from '../components/Footer/Footer.jsx';
 import { Builder, BuilderComponent } from '../components/Builder/builder.js'
 import registerComponents from '../components/Builder/register-components.js';
 
 export default function Body({ pages, page, children }) {
 	
-	registerComponents(Builder);
+	console.log('Builder page', page, children);
 
+	registerComponents(Builder);
+	
 	return (
 		<>
 			<HeaderBar pages={pages} page={page}/>
 
 			<main>
-				{children ? children : <BuilderComponent page={page}/>}
+				<BuilderComponent page={page}/>
 			</main>	
 			
 			<Footer pages={pages} page={page} />
