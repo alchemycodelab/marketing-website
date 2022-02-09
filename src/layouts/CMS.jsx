de4e8db67d8872653a1155cdd26a86064ee6ad1e
Marty Nelson <marty@martypdx.com>
2022-02-09 13:28:28 -0800

    fix css order issue in dev, but prod build gives seg fault

--- src/layouts/CMS.jsx
+++ src/layouts/CMS.jsx
@@ -1,10 +1,20 @@
-
 import { Builder, BuilderComponent } from '../components/Builder/builder.js'
 import registerComponents from '../components/Builder/register-components.js';
+import HeaderBar from '../components/HeaderBar/HeaderBar.jsx'
+import Footer from '../components/Footer/Footer.jsx';
 
 registerComponents(Builder);
 	
-export default function CMS({ page }) {
-  return <BuilderComponent model="page" content={page} />
-}
-	
+export default function CMS({ pages, page }) {
+  return (
+    <>
+      <HeaderBar pages={pages} page={page}/>
+
+        <main>		
+          <BuilderComponent model="page" content={page} />
+        </main>	
+
+      <Footer pages={pages} page={page} />
+    </>
+  );
+}  
