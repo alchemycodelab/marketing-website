d2cba7bfbdf93c488750aeca9103034fe97f8c8b
Marty Nelson <marty@martypdx.com>
2022-03-04 12:20:52 -0800

    try different config for CMS

--- astro.editing.config.mjs
+++ astro.editing.config.mjs
@@ -3,8 +3,10 @@ console.log('editing config');
 export default /** @type {import('astro').AstroUserConfig} */ ({
   buildOptions: {
     site: 'https://alchemy-marketing.netlify.app/builder-editing/',
+    // site: 'http://localhost:3000/builder-editing/',
     sitemap: false
   },
+  pages: './src/pages-client',
   dist: './dist/builder-editing',
   vite: {
     mode: 'production'
