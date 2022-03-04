57a57e5c22467725a11694a54fbd0696534f3c01
Marty Nelson <marty@martypdx.com>
2022-03-04 11:58:21 -0800

    changes that hopefully fix build issues

--- astro.editing.config.mjs
+++ astro.editing.config.mjs
@@ -2,7 +2,7 @@ console.log('editing config');
 // @ts-check
 export default /** @type {import('astro').AstroUserConfig} */ ({
   buildOptions: {
-    site: 'https://alchemy-marketing.netlify.app/builder-editing/',
+    // site: 'https://alchemy-marketing.netlify.app/builder-editing/',
     sitemap: false
   },
   dist: './dist/builder-editing',
