904e6b9ee98cf41d5fee3fc6ec8ccc9a09608953
Marty Nelson <marty@martypdx.com>
2022-03-04 12:34:07 -0800

    change config for cms to be separate netflify site

--- astro.cms.config.mjs
+++ astro.cms.config.mjs
@@ -1,13 +1,13 @@
-console.log('editing config');
+console.log('cms config');
 // @ts-check
 export default /** @type {import('astro').AstroUserConfig} */ ({
   buildOptions: {
-    site: 'https://alchemy-marketing.netlify.app/builder-editing/',
+    site: 'https://alchemy-marketing-cms.netlify.app/',
     // site: 'http://localhost:3000/builder-editing/',
     sitemap: false
   },
   pages: './src/pages-client',
-  dist: './dist/builder-editing',
+  // dist: './dist/builder-editing',
   vite: {
     mode: 'production'
   },
