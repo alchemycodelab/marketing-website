2e152cbb1f7adddf00eb135b0ba230e991bc39e8
Marty Nelson <marty@martypdx.com>
2022-04-14 15:17:10 -0700

    working dev build with astro beta 1

--- astro.dev.config.mjs
+++ astro.dev.config.mjs
@@ -1,14 +1,18 @@
+import { defineConfig } from 'astro/config'
+import react from '@astrojs/react';
+
 console.log('dev config');
-// @ts-check
-export default /** @type {import('astro').AstroUserConfig} */ ({
-  buildOptions: {
-    sitemap: false
-  },
-  devOptions: {
+
+export default defineConfig({
+  server: {
     port: Number(process.env.PORT || 3000),
     hostname: process.env.PORT ? '0.0.0.0' : 'localhost'
   },
-  pages: './src/pages-client',
+  // pages: './src/pages-client',
   // pages: './src/pages',
-  renderers: ['@astrojs/renderer-react'],
-});
+  integrations: [
+    react(),
+  ],
+})
+
+
