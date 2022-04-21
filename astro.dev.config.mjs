8eac97d92912f6cd33eb5d854ce4ba2686639756
Marty Nelson <marty@martypdx.com>
2022-04-21 16:48:25 -0700

    log out PORT

--- astro.dev.config.mjs
+++ astro.dev.config.mjs
@@ -4,10 +4,11 @@ import react from '@astrojs/react';
 console.log('dev config');
 
 const PORT = Number(process.env.PORT || 3000);
+console.log('port is', PORT);
 
 export default defineConfig({
   server: {
-    port: Number(process.env.PORT || 3000),
+    port: PORT,
     // hostname: process.env.PORT ? '0.0.0.0' : 'localhost'
   },
   // pages: './src/pages-client',
