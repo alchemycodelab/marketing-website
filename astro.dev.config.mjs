cfb17ce896ee0c39fefd6f809d3adc15238395eb
Marty Nelson <marty@martypdx.com>
2022-04-21 16:42:36 -0700

    don't change hostname on heroku

--- astro.dev.config.mjs
+++ astro.dev.config.mjs
@@ -3,10 +3,12 @@ import react from '@astrojs/react';
 
 console.log('dev config');
 
+const PORT = Number(process.env.PORT || 3000);
+
 export default defineConfig({
   server: {
     port: Number(process.env.PORT || 3000),
-    hostname: process.env.PORT ? '0.0.0.0' : 'localhost'
+    // hostname: process.env.PORT ? '0.0.0.0' : 'localhost'
   },
   // pages: './src/pages-client',
   // pages: './src/pages',
